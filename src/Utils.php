592edb1b0b7c9efdf5eb89604a8c0f8609d3581a
Matt Wolf <probie93@gmail.com>
2020-06-10 01:51:19 -0400

    Create Utils.php

---
+++ src/Utils.php
@@ -0,0 +1,25 @@
+<?php
+namespace Pkerrigan\Xray;
+
+/**
+ * 
+ * @author Patrick Kerrigan (patrickkerrigan.uk)
+ * @since 30/06/2019
+ * @internal
+ */
+class Utils
+{    
+    public static function sortSamplingRulesByPriorityDescending(array $samplingRules): array
+    {
+        usort($samplingRules, function($samplingRule, $samplingRuleOther) {
+            return $samplingRule['Priority'] - $samplingRuleOther['Priority'];
+        });
+        
+        return $samplingRules;
+    }
+    
+    public static function randomPossibility(int $percentage): bool
+    {
+        return random_int(0, 99) < $percentage;
+    }
+}
