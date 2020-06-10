95c9ccd5e0a3670d55c65ea9093dbfaa43bd9c74
Matt Wolf <probie93@gmail.com>
2020-06-10 01:53:45 -0400

    Create UtilsTest.php

---
+++ tests/UtilsTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace Pkerrigan\Xray;
+
+use PHPUnit\Framework\TestCase;
+
+class UtilsTest extends TestCase
+{    
+    /**
+     * 
+     * @dataProvider provideSortSamplingRulesByPriority
+     */
+    public function testSortSamplingRulesByPriority($samplingRules, $expected)
+    {
+        $this->assertEquals($expected, Utils::sortSamplingRulesByPriorityDescending($samplingRules));
+    }
+    
+    public function provideSortSamplingRulesByPriority()
+    {
+        return [
+            'Sort by priority descending' => [
+                [
+                    [
+                        'Priority' => 1000,
+                        'RuleName' => 'Default'
+                    ],
+                    [
+                        'Priority' => 1,
+                        'RuleName' => 'Important'                   
+                    ]
+                ],
+                [
+                    [
+                        'Priority' => 1,
+                        'RuleName' => 'Important'
+                    ],
+                    [
+                        'Priority' => 1000,
+                        'RuleName' => 'Default'
+                    ]
+                ]
+            ]
+        ];
+    }
+}
