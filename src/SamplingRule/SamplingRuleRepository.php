23d7fe5d47aa1ef21e637f00b2df4a84c6e4147c
Matt Wolf <probie93@gmail.com>
2020-06-10 01:49:23 -0400

    Create SamplingRuleRepository.php

---
+++ src/SamplingRule/SamplingRuleRepository.php
@@ -0,0 +1,14 @@
+<?php
+namespace Pkerrigan\Xray\SamplingRule;
+
+/**
+ * Responsible for retrieving sampling rules
+ *
+ * @author Niklas Ekman <nikl.ekman@gmail.com>
+ * @since 30/06/2019
+ * @see https://docs.aws.amazon.com/xray/latest/devguide/xray-api-sampling.html
+ */
+interface SamplingRuleRepository
+{
+    public function getAll(): array;
+}
